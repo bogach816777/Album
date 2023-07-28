b3db38b7a3a0200986b670616fc15a14b38eb0c0
Job <bogach816@example.com>
2023-07-28 11:33:51 +0300

    second

--- comments/src/Comments.jsx
+++ comments/src/Comments.jsx
@@ -1,45 +1,45 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 
 const Comments = () => {
+  const apiUrl = "https://jsonplaceholder.typicode.com/comments";
+  const [comments, setComments] = useState([]);
+  const [search, setSearch] = useState('')
 
- const apiUrl = "https://jsonplaceholder.typicode.com/comments";
- const [comments, setComments] = useState([])
- 
-useEffect(()=>{
-    fetchFunction()
-}, [])
+  useEffect(() => {
+    fetchFunction();
+  }, []);
 
- const fetchFunction = async()=>{
-    try{
-    const response = await fetch(apiUrl);
-    const data = await response.json();
-    console.log(data)
-    setComments(data)
-}catch(err){
-    console.error("Не працює Api");
-}
+  const fetchFunction = async () => {
+    try {
+      const response = await fetch(apiUrl);
+      const data = await response.json();
+      console.log(data);
+      setComments(data);
+    } catch (err) {
+      console.error("Не працює Api");
+    }
+  };
 
-
-
- }
-
+  const handleSearch = (event)=>{
+    setSearch(event.target.value);
+  }
+  const searchComments = comments.filter((comment)=>
+  comment.name.toLowerCase().includes(search.toLowerCase())
+  || comment.email.toLowerCase().includes(search.toLowerCase()))
   return (
-    <>
-    <div className='container'>
-        {comments.map((coment)=> (
-            <article className='container-body' key={coment.id}>
-                <h2>{coment.name}</h2>
-                <p>{coment.email}</p>
-                <p>{coment.body}</p>
-            </article>
-            
+    <div className='main'>
+    <input type="text" className='input' onChange={handleSearch} value={search} placeholder='Search by the name and email' />
+      <div className='container'>
+        {searchComments.map((comment) => (
+          <article className='container-body' key={comment.id}>
+            <h2>{comment.name}</h2>
+            <p>{comment.email}</p>
+            <p>{comment.body}</p>
+          </article>
         ))}
-
-
-      
+      </div>
     </div>
-    </>
-  )
-}
+  );
+};
 
-export default Comments
+export default Comments;
